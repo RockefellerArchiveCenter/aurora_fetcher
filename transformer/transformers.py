96f271ac2ba350c23d41393f67f58f97ab4cbbb5
Hillel Arnold <helrond@hotmail.com>
2018-05-14 21:42:59 -0400

    transform rights

--- transformer/transformers.py
+++ transformer/transformers.py
@@ -85,10 +85,47 @@ class ArchivesSpaceDataTransformer(object):
                 linked_agents.append({"role": "creator", "terms": [], "ref": agent_ref})
         return linked_agents
 
+    def transform_rights_acts(self, rights_granted):
+        acts = []
+        for granted in rights_granted:
+            act = {
+                "notes": [
+                    {"jsonmodel_type": "note_rights_statement_act",
+                     "type": "additional_information", "content": [granted['note']]}],
+                "act_type": granted['act'],
+                "restriction": granted['restriction'],
+                "start_date": granted['start_date'],
+                "end_date": granted['end_date'],
+            }
+            acts.append(act)
+        return acts
+
     def transform_rights(self):
         rights_statements = []
         for r in self.data['rights_statements']:
-            statement = None
+            statement = {
+                "rights_type": r['rights_basis'].lower(),
+                "start_date": r['start_date'],
+                "end_date": r['end_date'],
+                "notes": [
+                    {"jsonmodel_type": "note_rights_statement",
+                     "type": "type_note", "content": [r['note']]}],
+                "acts": self.transform_rights_acts(r['rights_granted']),
+                "external_documents": [],
+                "linked_agents": [],
+            }
+            if 'status' in r:
+                statement = {**statement, "status": r['status']}
+            if 'determination_date' in r:
+                statement = {**statement, "determination_date": r['determination_date']}
+            if 'terms' in r:
+                statement = {**statement, "license_terms": r['terms']}
+            if 'citation' in r:
+                statement = {**statement, "statute_citation": r['citation']}
+            if 'jurisdiction' in r:
+                statement = {**statement, "jurisdiction": r['jurisdiction'].upper()}
+            if 'other_rights_basis' in r:
+                statement = {**statement, "other_rights_basis": r['other_rights_basis'].lower()}
             rights_statements.append(statement)
         return rights_statements
 
@@ -111,30 +148,24 @@ class ArchivesSpaceDataTransformer(object):
             "external_documents": [], "instances": [], "subjects": []
             }
         try:
-            title = self.metadata['title']
-            language = self.transform_langcode(self.metadata['language'])
-            external_ids = self.transform_external_ids(self.data['url'])
-            extents = self.transform_extents(
-                {"bytes": self.metadata['payload_oxum'].split(".")[0],
-                 "files": self.metadata['payload_oxum'].split(".")[1]})
-            dates = self.transform_dates(self.metadata['date_start'], self.metadata['date_end'])
-            scopecontent = self.transform_scopecontent(self.metadata['internal_sender_description'])
-            langmaterial = self.transform_langnote(self.metadata['language'])
-            repository_ref = {"ref": "/repositories/{}".format(settings.ARCHIVESSPACE['repo_id'])}
-            resource_ref = {'ref': self.data['collection']}
-            rights_statements = self.transform_rights()
-            linked_agents = self.transform_linked_agents()
-
-            self.consumer_data = {**defaults, "title": title, "language": language,
-                "external_ids": external_ids, "extents": extents,
-                "dates": dates, "rights_statements": rights_statements,
-                "linked_agents": linked_agents, "resource": resource_ref,
-                "repository": repository_ref, "notes": [scopecontent, langmaterial]}
-
+            self.consumer_data = {
+                **defaults,
+                "title": self.metadata['title'],
+                "language": self.transform_langcode(self.metadata['language']),
+                "external_ids": self.transform_external_ids(self.data['url']),
+                "extents": self.transform_extents(
+                    {"bytes": self.metadata['payload_oxum'].split(".")[0],
+                     "files": self.metadata['payload_oxum'].split(".")[1]}),
+                "dates": self.transform_dates(self.metadata['date_start'], self.metadata['date_end']),
+                "rights_statements": self.transform_rights(),
+                "linked_agents": self.transform_linked_agents(),
+                "resource": {'ref': self.data['collection']},
+                "repository": {"ref": "/repositories/{}".format(settings.ARCHIVESSPACE['repo_id'])},
+                "notes": [
+                    self.transform_scopecontent(self.metadata['internal_sender_description']),
+                    self.transform_langnote(self.metadata['language'])]}
             if 'parent' in self.data:
-                parent_ref = {"ref": self.data['parent']}
-                self.consumer_data = {**self.consumer_data, "parent": parent_ref}
-
+                self.consumer_data = {**self.consumer_data, "parent": {"ref": self.data['parent']}}
             return True
         except Exception as e:
             print(e)
