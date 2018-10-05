eea41d73f3d138af302a79085a4518ecb6696d65
Hillel Arnold <helrond@hotmail.com>
2018-10-04 22:19:00 -0400

    refizactoring

--- transformer/clients.py
+++ transformer/clients.py
@@ -16,12 +16,6 @@ logger.setLevel(logging.DEBUG)
 logger = wrap_logger(logger)
 
 
-class ArchivesSpaceClientError(Exception): pass
-
-
-class UrsaMajorClientError(Exception): pass
-
-
 class ArchivesSpaceClient(object):
 
     def __init__(self):
@@ -31,14 +25,12 @@ class ArchivesSpaceClient(object):
             username=settings.ARCHIVESSPACE['username'],
             password=settings.ARCHIVESSPACE['password'],
         )
+        self.repo_id = settings.ARCHIVESSPACE['repo_id']
         if not self.client.authorize():
             self.log.error(
                 "Couldn't authenticate user credentials for ArchivesSpace",
                 object=settings.ARCHIVESSPACE['username'])
-            raise ArchivesSpaceClientError(
-                "Couldn't authenticate user credentials for ArchivesSpace",
-                object=settings.ARCHIVESSPACE['username'])
-        self.repo_id = settings.ARCHIVESSPACE['repo_id']
+            return False
 
     def create(self, data, type, *args, **kwargs):
         self.log = self.log.bind(request_id=str(uuid4()))
@@ -49,12 +41,13 @@ class ArchivesSpaceClient(object):
             'organization': 'agents/corporate_entities',
             'family': 'agents/families',
         }
-        resp = self.client.post(ENDPOINTS[type], data=json.dumps(data), *args, **kwargs)
-        if resp.status_code != 200:
-            self.log.error('Error creating object in ArchivesSpace: {msg}'.format(msg=resp.json()['error']))
-            raise ArchivesSpaceClientError('Error creating object in ArchivesSpace: {msg}'.format(msg=resp.json()['error']))
-        self.log.debug("Object created in Archivesspace", object=resp.json()['uri'])
-        return resp.json()['uri']
+        try:
+            resp = self.client.post(ENDPOINTS[type], data=json.dumps(data), *args, **kwargs)
+            self.log.debug("Object created in Archivesspace", object=resp.json()['uri'])
+            return resp.json()['uri']
+        except Exception as e:
+            self.log.error('Error creating object in ArchivesSpace: {}'.format(e))
+            return False
 
     def get_or_create(self, type, field, value, last_updated, consumer_data):
         self.log = self.log.bind(request_id=str(uuid4()))
@@ -69,31 +62,30 @@ class ArchivesSpaceClient(object):
         model_type = [t[1] for t in TYPE_LIST if t[0] == type][0]
         endpoint = [t[2] for t in TYPE_LIST if t[0] == type][0]
         query = json.dumps({"query": {"field": field, "value": value, "jsonmodel_type": "field_query"}})
-        resp = self.client.get('search', params={"page": 1, "type[]": model_type, "aq": query})
-        if resp.status_code != 200:
-            self.log.error('Error searching for agent: {msg}'.format(msg=resp.json()['error']))
-            raise ArchivesSpaceClientError('Error searching for agent: {msg}'.format(msg=resp.json()['error']))
-        if len(resp.json()['results']) == 0:
-            resp = self.client.get(endpoint, params={"all_ids": True, "modified_since": last_updated-120})
-            if resp.status_code != 200:
-                self.log.error('Error getting updated agents: {msg}'.format(msg=resp.json()['error']))
-                raise ArchivesSpaceClientError('Error getting updated agents: {msg}'.format(msg=resp.json()['error']))
-            for ref in resp.json():
-                resp = self.client.get('{}/{}'.format(endpoint, ref))
-                if resp.json()[field] == str(value):
-                    return resp.json()['uri']
-            self.log.debug("No match for object found in ArchivesSpace", object=value)
-            return self.create(consumer_data, type)
-        return resp.json()['results'][0]['uri']
+        try:
+            resp = self.client.get('search', params={"page": 1, "type[]": model_type, "aq": query})
+            if len(resp.json()['results']) == 0:
+                resp = self.client.get(endpoint, params={"all_ids": True, "modified_since": last_updated-120})
+                for ref in resp.json():
+                    resp = self.client.get('{}/{}'.format(endpoint, ref))
+                    if resp.json()[field] == str(value):
+                        return resp.json()['uri']
+                self.log.debug("No match for object found in ArchivesSpace", object=value)
+                return self.create(consumer_data, type)
+            return resp.json()['results'][0]['uri']
+        except Exception as e:
+            self.log.error('Error finding or creating object in ArchivesSpace: {}'.format(e))
+            return False
 
     def retrieve(self, url, *args, **kwargs):
         self.log = self.log.bind(request_id=str(uuid4()))
-        resp = self.client.get(url, *args, **kwargs)
-        if resp.status_code != 200:
-            self.log.error('Error retrieving object from ArchivesSpace: {msg}'.format(msg=resp.json()['error']))
-            raise ArchivesSpaceClientError('Error retrieving object from ArchivesSpace: {msg}'.format(msg=resp.json()['error']))
-        self.log.debug("Updated accessions retrieved from Archivesspace")
-        return resp.json()
+        try:
+            resp = self.client.get(url, *args, **kwargs)
+            self.log.debug("Updated accessions retrieved from Archivesspace")
+            return resp.json()
+        except Exception as e:
+            self.log.error('Error retrieving object from ArchivesSpace: {}'.format(e))
+            return False
 
 
 class UrsaMajorClient(object):
@@ -120,15 +112,16 @@ class UrsaMajorClient(object):
 
     def retrieve(self, url, *args, **kwargs):
         self.log = self.log.bind(request_id=str(uuid4()))
-        # TODO: move URL manipulation to client library
-        url = urlparse(url)
-        full_url = "/".join([self.baseurl.rstrip("/"), url.path.lstrip("/")])
-        resp = self.client.get(full_url, *args, **kwargs)
-        if resp.status_code != 200:
-            self.log.error("Error retrieving data from Ursa Major: {msg}".format(msg=resp.json()['detail']))
-            raise UrsaMajorClientError("Error retrieving data from Ursa Major: {msg}".format(msg=resp.json()['detail']))
-        self.log.debug("Object retrieved from Ursa Major", object=url)
-        return resp.json()
+        try:
+            # TODO: move URL manipulation to client library
+            url = urlparse(url)
+            full_url = "/".join([self.baseurl.rstrip("/"), url.path.lstrip("/")])
+            resp = self.client.get(full_url, *args, **kwargs)
+            self.log.debug("Object retrieved from Ursa Major", object=url)
+            return resp.json()
+        except Exception as e:
+            self.log.error("Error retrieving data from Ursa Major: {}".format(e))
+            return False
 
     def retrieve_paged(self, url, *args, **kwargs):
         self.log = self.log.bind(request_id=str(uuid4()))
@@ -140,35 +133,34 @@ class UrsaMajorClient(object):
             return resp
         except Exception as e:
             self.log.error("Error retrieving list from Ursa Major: {}".format(e))
-            raise UrsaMajorClientError(e)
+            return False
 
     def update(self, url, data, *args, **kwargs):
         self.log = self.log.bind(request_id=str(uuid4()))
-        # TODO: move URL manipulation to client library
-        url = urlparse(url)
-        full_url = "/".join([self.baseurl.rstrip("/"), url.path.lstrip("/")])
-        resp = self.client.put(full_url, data=json.dumps(data), headers={"Content-Type":"application/json"}, *args, **kwargs)
-        if resp.status_code != 200:
-            self.log.error("Error saving data in Ursa Major: {msg}".format(msg=resp.json()['detail']))
-            raise UrsaMajorClientError("Error saving data in Ursa Major: {msg}".format(msg=resp.json()['detail']))
-        self.log.debug("Object saved in Ursa Major", object=url)
-        return resp.json()
+        try:
+            # TODO: move URL manipulation to client library
+            url = urlparse(url)
+            full_url = "/".join([self.baseurl.rstrip("/"), url.path.lstrip("/")])
+            resp = self.client.put(full_url, data=json.dumps(data), headers={"Content-Type":"application/json"}, *args, **kwargs)
+            self.log.debug("Object saved in Ursa Major", object=full_url)
+            return resp.json()
+        except Exception as e:
+            self.log.error("Error updating object in Ursa Major: {}".format(e))
+            return False
 
     def find_bag_by_id(self, identifier, *args, **kwargs):
         self.log = self.log.bind(request_id=str(uuid4()))
-        # TODO: move URL manipulation to client library
-        full_url = "/".join([self.baseurl.rstrip("/"), 'bags/?id={}'.format(identifier)])
-        object = self.client.get(full_url, *args, **kwargs)
-        if object.status_code != 200:
-            self.log.error("Error retrieving data from Ursa Major: {msg}".format(msg=resp.json()['detail']))
-            raise UrsaMajorClientError("Error retrieving data from Ursa Major: {msg}".format(msg=resp.json()['detail']))
-        bag = object.json()[0]
-        object_url = bag['url']
-        object_url = urlparse(object_url)
-        full_url = "/".join([self.baseurl.rstrip("/"), object_url.path.lstrip("/")])
-        resp = self.client.get(full_url, *args, **kwargs)
-        if object.status_code != 200:
-            self.log.error("Error retrieving data from Ursa Major: {msg}".format(msg=resp.json()['detail']))
-            raise UrsaMajorClientError("Error retrieving data from Ursa Major: {msg}".format(msg=resp.json()['detail']))
-        self.log.debug("Object retrieved from Ursa Major", object=identifier)
-        return resp.json()
+        try:
+            # TODO: move URL manipulation to client library
+            full_url = "/".join([self.baseurl.rstrip("/"), 'bags/?id={}'.format(identifier)])
+            bag_resp = self.client.get(full_url, *args, **kwargs)
+            bag_url = bag_resp.json()[0]['url']
+            # TODO move URL manipulation to client library
+            object_url = urlparse(bag_url)
+            full_url = "/".join([self.baseurl.rstrip("/"), object_url.path.lstrip("/")])
+            resp = self.client.get(full_url, *args, **kwargs)
+            self.log.debug("Object retrieved from Ursa Major", object=full_url)
+            return resp.json()
+        except Exception as e:
+            self.log.error("Error finding bag by id: {}".format(e))
+            return False
--- transformer/routines.py
+++ transformer/routines.py
@@ -16,6 +16,9 @@ logger.setLevel(logging.DEBUG)
 logger = wrap_logger(logger)
 
 
+class TransferRoutineError(Exception): pass
+
+
 class TransferRoutine:
     def __init__(self, aspace_client=None, ursa_major_client=None):
         self.aspace_client = aspace_client if aspace_client else ArchivesSpaceClient()
@@ -27,36 +30,45 @@ class TransferRoutine:
         self.log.bind(request_id=str(uuid4()))
 
         if int(transfer.process_status) <= 20:
-            transfer_data = self.ursa_major_client.find_bag_by_id(transfer.identifier)
-            transfer.transfer_data = transfer_data
-            accession_data = self.ursa_major_client.retrieve(transfer.transfer_data['accession'])
-            transfer.accession_data = accession_data
-            if not transfer.accession_data.get('archivesspace_identifier'):
-                transformed_data = self.transformer.transform_accession(transfer.accession_data['data'])
-                accession_identifier = self.aspace_client.create(transformed_data, 'accession')
-                transfer.accession_data['archivesspace_identifier'] = accession_identifier
-                updated = self.ursa_major_client.update(transfer.accession_data['url'], data=transfer.accession_data)
-            transfer.process_status = 20
-            transfer.save()
+            try:
+                transfer_data = self.ursa_major_client.find_bag_by_id(transfer.identifier)
+                transfer.transfer_data = transfer_data
+                accession_data = self.ursa_major_client.retrieve(transfer.transfer_data['accession'])
+                transfer.accession_data = accession_data
+                if not transfer.accession_data.get('archivesspace_identifier'):
+                    transformed_data = self.transformer.transform_accession(transfer.accession_data['data'])
+                    accession_identifier = self.aspace_client.create(transformed_data, 'accession')
+                    transfer.accession_data['archivesspace_identifier'] = accession_identifier
+                    updated = self.ursa_major_client.update(transfer.accession_data['url'], data=transfer.accession_data)
+                transfer.process_status = 20
+                transfer.save()
+            except Exception as e:
+                raise TransferRoutineError("Error with accession: {}".format(e))
 
         if int(transfer.process_status) <= 30:
-            if not transfer.transfer_data.get('archivesspace_parent_identifier'):
-                transformed_data = self.transformer.transform_grouping_component(transfer.accession_data['data'])
-                self.transformer.parent = self.aspace_client.create(transformed_data, 'component')
-                for t in transfer.accession_data['data']['transfers']:
-                    data = self.ursa_major_client.find_bag_by_id(t['identifier'])
-                    data['archivesspace_parent_identifier'] = self.transformer.parent
-                    self.ursa_major_client.update(data['url'], data=data)
-            else:
-                self.transformer.parent = transfer.transfer_data['archivesspace_parent_identifier']
-            self.transformer.resource = transfer.accession_data['data']['resource']
-            transfer.process_status = 30
-            transfer.save()
+            try:
+                if not transfer.transfer_data.get('archivesspace_parent_identifier'):
+                    transformed_data = self.transformer.transform_grouping_component(transfer.accession_data['data'])
+                    self.transformer.parent = self.aspace_client.create(transformed_data, 'component')
+                    for t in transfer.accession_data['data']['transfers']:
+                        data = self.ursa_major_client.find_bag_by_id(t['identifier'])
+                        data['archivesspace_parent_identifier'] = self.transformer.parent
+                        self.ursa_major_client.update(data['url'], data=data)
+                else:
+                    self.transformer.parent = transfer.transfer_data['archivesspace_parent_identifier']
+                self.transformer.resource = transfer.accession_data['data']['resource']
+                transfer.process_status = 30
+                transfer.save()
+            except Exception as e:
+                raise TransferRoutineError("Error with grouping component: {}".format(e))
 
         if int(transfer.process_status) <= 40:
-            transformed_data = self.transformer.transform_component(transfer.transfer_data['data'])
-            transfer_identifier = self.aspace_client.create(transformed_data, 'component')
-            transfer.transfer_data['archivesspace_identifier'] = transfer_identifier
-            self.ursa_major_client.update(transfer.transfer_data['url'], data=transfer.transfer_data)
-            transfer.process_status = 40
-            transfer.save()
+            try:
+                transformed_data = self.transformer.transform_component(transfer.transfer_data['data'])
+                transfer_identifier = self.aspace_client.create(transformed_data, 'component')
+                transfer.transfer_data['archivesspace_identifier'] = transfer_identifier
+                self.ursa_major_client.update(transfer.transfer_data['url'], data=transfer.transfer_data)
+                transfer.process_status = 40
+                transfer.save()
+            except Exception as e:
+                raise TransferRoutineError("Error with transfer component: {}".format(e))
