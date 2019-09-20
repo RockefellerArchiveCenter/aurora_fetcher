3a490124b85c00219acb88f0b4e53e929b47e5c1
Hillel Arnold <helrond@hotmail.com>
2019-09-19 20:14:28 -0400

    refactors clients, fixes #75

--- transformer/clients.py
+++ transformer/clients.py
@@ -19,6 +19,16 @@ class ArchivesSpaceClient(object):
         if not self.client.authorize():
             raise ArchivesSpaceClientError("Couldn't authenticate user credentials for ArchivesSpace")
 
+    def send_request(self, method, url, data=None, *args, **kwargs):
+        r = getattr(self.client, method)(url, data=json.dumps(data), *args, **kwargs)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            raise ArchivesSpaceClientError('Error sending {} request to {}: {}'.format(method, url, r.json()['error']))
+
+    def retrieve(self, url, *args, **kwargs):
+        return self.send_request('get', url, *args, **kwargs)
+
     def create(self, data, type, *args, **kwargs):
         ENDPOINTS = {
             'component': 'repositories/{repo_id}/archival_objects'.format(repo_id=self.repo_id),
@@ -28,24 +38,10 @@ class ArchivesSpaceClient(object):
             'organization': 'agents/corporate_entities',
             'family': 'agents/families',
         }
-        resp = self.client.post(ENDPOINTS[type], data=json.dumps(data), *args, **kwargs)
-        if resp.status_code == 200:
-            return resp.json()['uri']
-        else:
-            if resp.json()['error'].get('id_0'):
-                raise ArchivesSpaceClientAccessionNumberError(resp.json()['error'])
-            else:
-                raise ArchivesSpaceClientError(resp.json()['error'])
+        return self.send_request('post', ENDPOINTS[type], data, *args, **kwargs)
 
     def update(self, uri, data, *args, **kwargs):
-        try:
-            resp = self.client.post(uri, data=json.dumps(data), *args, **kwargs)
-            if resp.status_code == 200:
-                return resp.json()['uri']
-            else:
-                raise ArchivesSpaceClientError('Error updating object in ArchivesSpace: {}'.format(resp.json()['error']))
-        except Exception as e:
-            raise ArchivesSpaceClientError('Error updating object in ArchivesSpace: {}'.format(e))
+        return self.send_request('post', uri, data, *args, *kwargs)
 
     def get_or_create(self, type, field, value, last_updated, consumer_data):
         TYPE_LIST = (
@@ -60,38 +56,28 @@ class ArchivesSpaceClient(object):
         endpoint = [t[2] for t in TYPE_LIST if t[0] == type][0]
         query = json.dumps({"query": {"field": field, "value": value, "jsonmodel_type": "field_query"}})
         try:
-            resp = self.client.get('search', params={"page": 1, "type[]": model_type, "aq": query})
-            if len(resp.json()['results']) == 0:
-                resp = self.client.get(endpoint, params={"all_ids": True, "modified_since": last_updated-120})
-                for ref in resp.json():
-                    resp = self.client.get('{}/{}'.format(endpoint, ref))
-                    if resp.json()[field] == str(value):
-                        return resp.json()['uri']
+            r = self.client.get('search', params={"page": 1, "type[]": model_type, "aq": query}).json()
+            if len(r['results']) == 0:
+                r = self.client.get(endpoint, params={"all_ids": True, "modified_since": last_updated-120}).json()
+                for ref in r:
+                    r = self.client.get('{}/{}'.format(endpoint, ref)).json()
+                    if r[field] == str(value):
+                        return r['uri']
                 return self.create(consumer_data, type)
-            return resp.json()['results'][0]['uri']
+            return r['results'][0]['uri']
         except Exception as e:
             raise ArchivesSpaceClientError('Error finding or creating object in ArchivesSpace: {}'.format(e))
-
-    def retrieve(self, url, *args, **kwargs):
-        try:
-            resp = self.client.get(url, *args, **kwargs)
-            if resp.status_code == 200:
-                return resp.json()
-            else:
-                raise ArchivesSpaceClientError('Error retrieving object from ArchivesSpace: {}'.format(resp.json()['error']))
-        except Exception as e:
-            raise ArchivesSpaceClientError('Error retrieving object from ArchivesSpace: {}'.format(e))
 
     def next_accession_number(self):
         current_year = str(date.today().year)
         try:
             query = json.dumps({"query": {"field": "four_part_id", "value": current_year, "jsonmodel_type": "field_query"}})
-            resp = self.client.get('search', params={"page": 1, "type[]": "accession", "sort": "identifier desc", "aq": query}).json()
-            if resp.get('total_hits') < 1:
+            r = self.client.get('search', params={"page": 1, "type[]": "accession", "sort": "identifier desc", "aq": query}).json()
+            if r.get('total_hits') < 1:
                 return [current_year, "001"]
             else:
-                if resp['results'][0]['identifier'].split("-")[0] == current_year:
-                    id_1 = int(resp['results'][0]['identifier'].split("-")[1])
+                if r['results'][0]['identifier'].split("-")[0] == current_year:
+                    id_1 = int(r['results'][0]['identifier'].split("-")[1])
                     id_1 += 1
                     updated = str(id_1).zfill(3)
                     return [current_year, updated]
@@ -107,12 +93,17 @@ class UrsaMajorClient(object):
     def __init__(self, baseurl):
         self.client = ElectronBond(baseurl=baseurl)
 
-    def retrieve(self, url, *args, **kwargs):
+    def send_request(self, method, url, data=None, *args, **kwargs):
         try:
-            resp = self.client.get(url, *args, **kwargs)
-            return resp.json()
+            return getattr(self.client, method)(url, data=json.dumps(data), *args, **kwargs).json()
         except Exception as e:
-            raise UrsaMajorClientError("Error retrieving data from Ursa Major: {}".format(e))
+            raise UrsaMajorClientError("Error sending {} request to {}: {}".format(method, url, e))
+
+    def retrieve(self, url, *args, **kwargs):
+        return self.send_request('get', url, *args, **kwargs)
+
+    def update(self, url, data, *args, **kwargs):
+        return self.send_request('put', url, data, headers={"Content-Type":"application/json"}, *args, **kwargs)
 
     def retrieve_paged(self, url, *args, **kwargs):
         try:
@@ -121,13 +112,6 @@ class UrsaMajorClient(object):
         except Exception as e:
             raise UrsaMajorClientError("Error retrieving list from Ursa Major: {}".format(e))
 
-    def update(self, url, data, *args, **kwargs):
-        try:
-            resp = self.client.put(url, data=json.dumps(data), headers={"Content-Type":"application/json"}, *args, **kwargs)
-            return resp.json()
-        except Exception as e:
-            raise UrsaMajorClientError("Error updating object in Ursa Major: {}".format(e))
-
     def find_bag_by_id(self, identifier, *args, **kwargs):
         try:
             bag_resp = self.client.get("bags/", params={"id": identifier}).json()
@@ -135,8 +119,7 @@ class UrsaMajorClient(object):
             if count != 1:
                 raise UrsaMajorClientError("Found {} bags matching id {}, expected 1".format(count, identifier))
             bag_url = bag_resp.get('results')[0]['url']
-            resp = self.client.get(bag_url, *args, **kwargs).json()
-            return resp
+            return self.send_request('get', bag_url)
         except Exception as e:
             raise UrsaMajorClientError("Error finding bag by id: {}".format(e))
 
