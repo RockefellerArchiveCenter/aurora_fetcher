fa98744f7fefbe47c335e18e06d5e6532934f7f3
Hillel Arnold <helrond@hotmail.com>
2020-04-10 12:02:31 -0500

    move exception handling to client

--- transformer/clients.py
+++ transformer/clients.py
@@ -9,10 +9,6 @@ class ArchivesSpaceClientError(Exception):
     pass
 
 
-class ArchivesSpaceClientAccessionNumberError(Exception):
-    pass
-
-
 class UrsaMajorClientError(Exception):
     pass
 
@@ -30,12 +26,12 @@ class ArchivesSpaceClient(object):
         if not self.client.authorize():
             raise ArchivesSpaceClientError("Couldn't authenticate user credentials for ArchivesSpace")
         self.TYPE_LIST = {
-            'family': ['agent_family', 'agents/families'],
-            'organization': ['agent_corporate_entity', 'agents/corporate_entities'],
-            'person': ['agent_person', 'agents/people'],
-            'component': ['archival_object', 'repositories/{repo_id}/archival_objects'.format(repo_id=self.repo_id)],
-            'accession': ['accession', 'repositories/{repo_id}/accessions'.format(repo_id=self.repo_id)],
-            'digital object': ['digital_objects', 'repositories/{repo_id}/digital_objects'.format(repo_id=self.repo_id)]
+            "family": ["agent_family", "agents/families"],
+            "organization": ["agent_corporate_entity", "agents/corporate_entities"],
+            "person": ["agent_person", "agents/people"],
+            "component": ["archival_object", "repositories/{repo_id}/archival_objects".format(repo_id=self.repo_id)],
+            "accession": ["accession", "repositories/{repo_id}/accessions".format(repo_id=self.repo_id)],
+            "digital object": ["digital_objects", "repositories/{repo_id}/digital_objects".format(repo_id=self.repo_id)]
         }
 
     def send_request(self, method, url, data=None, **kwargs):
@@ -44,23 +40,22 @@ class ArchivesSpaceClient(object):
         if r.status_code == 200:
             return r.json()
         else:
-            if r.json()['error'].get('id_0'):
+            if r.json()["error"].get("id_0"):
                 """Account for indexing delays by bumping up to the next accession number."""
-                id_1 = int(data['id_1'])
+                id_1 = int(data["id_1"])
                 id_1 += 1
-                data['id_1'] = str(id_1).zfill(3)
-                self.aspace_client.create(data, 'accession').get('uri')
-                return self.create(data, 'accession')
-            raise ArchivesSpaceClientError('Error sending {} request to {}: {}'.format(method, url, r.json()['error']))
+                data["id_1"] = str(id_1).zfill(3)
+                return self.create(data, "accession")
+            raise ArchivesSpaceClientError("Error sending {} request to {}: {}".format(method, url, r.json()["error"]))
 
     def retrieve(self, url, **kwargs):
-        return self.send_request('get', url, **kwargs)
+        return self.send_request("get", url, **kwargs)
 
     def create(self, data, type, **kwargs):
-        return self.send_request('post', self.TYPE_LIST[type][1], data, **kwargs)
+        return self.send_request("post", self.TYPE_LIST[type][1], data, **kwargs)
 
     def update(self, uri, data, **kwargs):
-        return self.send_request('post', uri, data, **kwargs)
+        return self.send_request("post", uri, data, **kwargs)
 
     def get_or_create(self, type, field, value, last_updated, consumer_data):
         """
@@ -71,17 +66,17 @@ class ArchivesSpaceClient(object):
         endpoint = self.TYPE_LIST[type][1]
         query = json.dumps({"query": {"field": field, "value": value, "jsonmodel_type": "field_query"}})
         try:
-            r = self.client.get('search', params={"page": 1, "type[]": model_type, "aq": query}).json()
-            if len(r['results']) == 0:
+            r = self.client.get("search", params={"page": 1, "type[]": model_type, "aq": query}).json()
+            if len(r["results"]) == 0:
                 r = self.client.get(endpoint, params={"all_ids": True, "modified_since": last_updated - 120}).json()
                 for ref in r:
-                    r = self.client.get('{}/{}'.format(endpoint, ref)).json()
+                    r = self.client.get("{}/{}".format(endpoint, ref)).json()
                     if r[field] == str(value):
-                        return r['uri']
-                return self.create(consumer_data, type)
-            return r['results'][0]['uri']
+                        return r["uri"]
+                return self.create(consumer_data, type).get("uri")
+            return r["results"][0]["uri"]
         except Exception as e:
-            raise ArchivesSpaceClientError('Error finding or creating object in ArchivesSpace: {}'.format(e))
+            raise ArchivesSpaceClientError("Error finding or creating object in ArchivesSpace: {}".format(e))
 
     def next_accession_number(self):
         """
@@ -94,16 +89,16 @@ class ArchivesSpaceClient(object):
         current_year = str(date.today().year)
         try:
             query = json.dumps({"query": {"field": "four_part_id", "value": current_year, "jsonmodel_type": "field_query"}})
-            r = self.client.get('search', params={"page": 1, "type[]": "accession", "sort": "identifier desc", "aq": query}).json()
+            r = self.client.get("search", params={"page": 1, "type[]": "accession", "sort": "identifier desc", "aq": query}).json()
             number = "1"
-            if r.get('total_hits') >= 1:
-                if r['results'][0]['identifier'].split("-")[0] == current_year:
-                    id_1 = int(r['results'][0]['identifier'].split("-")[1])
+            if r.get("total_hits") >= 1:
+                if r["results"][0]["identifier"].split("-")[0] == current_year:
+                    id_1 = int(r["results"][0]["identifier"].split("-")[1])
                     id_1 += 1
                     number = str(id_1).zfill(3)
             return ":".join([current_year, number.zfill(3)])
         except Exception as e:
-            raise ArchivesSpaceClientError('Error retrieving next accession number from ArchivesSpace: {}'.format(e))
+            raise ArchivesSpaceClientError("Error retrieving next accession number from ArchivesSpace: {}".format(e))
 
 
 class UrsaMajorClient(object):
@@ -120,10 +115,10 @@ class UrsaMajorClient(object):
             raise UrsaMajorClientError("Error sending {} request to {}: {}".format(method, url, e))
 
     def retrieve(self, url, *args, **kwargs):
-        return self.send_request('get', url, **kwargs)
+        return self.send_request("get", url, **kwargs)
 
     def update(self, url, data, **kwargs):
-        return self.send_request('put', url, data, headers={"Content-Type": "application/json"}, **kwargs)
+        return self.send_request("put", url, data, headers={"Content-Type": "application/json"}, **kwargs)
 
     def retrieve_paged(self, url, **kwargs):
         try:
@@ -136,16 +131,17 @@ class UrsaMajorClient(object):
         """Finds a bag by its id."""
         try:
             bag_resp = self.client.get("bags/", params={"id": identifier}).json()
-            count = bag_resp.get('count')
+            count = bag_resp.get("count")
             if count != 1:
                 raise UrsaMajorClientError("Found {} bags matching id {}, expected 1".format(count, identifier))
-            bag_url = bag_resp.get('results')[0]['url']
-            return self.send_request('get', bag_url)
+            bag_url = bag_resp.get("results")[0]["url"]
+            return self.send_request("get", bag_url)
         except Exception as e:
             raise UrsaMajorClientError("Error finding bag by id: {}".format(e))
 
 
 class AuroraClient:
+    """Client to update data in Aurora."""
 
     def __init__(self, baseurl, username, password):
         self.client = ElectronBond(baseurl=baseurl, username=username, password=password)
