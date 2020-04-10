f48ad1ed8ccef7554a731bd8b7a292ff848f84d1
Hillel Arnold <helrond@hotmail.com>
2020-04-10 12:06:15 -0500

    debug routines

--- transformer/routines.py
+++ transformer/routines.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from aquarius import settings
 from odin.codecs import json_codec
@@ -7,9 +8,10 @@ from .clients import ArchivesSpaceClient, AuroraClient, UrsaMajorClient
 from .mappings import (SourceAccessionToArchivesSpaceAccession,
                        SourceAccessionToGroupingComponent,
                        SourcePackageToDigitalObject,
-                       SourceTransferToTransferComponent)
+                       SourceTransferToTransferComponent, map_agents)
 from .models import Package
-from .resources.source import SourceAccession, SourcePackage, SourceTransfer
+from .resources.source import (SourceAccession, SourceCreator, SourcePackage,
+                               SourceTransfer)
 
 
 class RoutineError(Exception):
@@ -21,8 +23,7 @@ class UpdateRequestError(Exception):
 
 
 class Routine:
-    """
-    Base class which is inherited by all other routines.
+    """Base routine class which is inherited by all other routines.
 
     Provides default clients for ArchivesSpace and Ursa Major, and instantiates
     a DataTransformer class.
@@ -40,11 +41,12 @@ class Routine:
     """
 
     def __init__(self):
-        self.aspace_client = ArchivesSpaceClient(settings.ARCHIVESSPACE['baseurl'],
-                                                 settings.ARCHIVESSPACE['username'],
-                                                 settings.ARCHIVESSPACE['password'],
-                                                 settings.ARCHIVESSPACE['repo_id'])
-        self.ursa_major_client = UrsaMajorClient(settings.URSA_MAJOR['baseurl'])
+        self.aspace_client = ArchivesSpaceClient(settings.ARCHIVESSPACE["baseurl"],
+                                                 settings.ARCHIVESSPACE["username"],
+                                                 settings.ARCHIVESSPACE["password"],
+                                                 settings.ARCHIVESSPACE["repo_id"])
+        self.ursa_major_client = UrsaMajorClient(settings.URSA_MAJOR["baseurl"])
+        self.start_time = int(time.time())
 
     def run(self):
         package_ids = []
@@ -70,10 +72,19 @@ class Routine:
         from_obj = json_codec.loads(json.dumps(data), resource=from_resource)
         return json.loads(json_codec.dumps(mapping.apply(from_obj)))
 
+    def get_linked_agents(self, agents):
+        linked_agents = []
+        for agent in agents:
+            agent_data = map_agents(SourceCreator(type=agent["type"], name=agent["name"]))
+            agent_ref = self.aspace_client.get_or_create(
+                agent["type"], "title", agent["name"],
+                self.start_time, agent_data)
+            linked_agents.append({"uri": agent_ref})
+        return linked_agents
+
 
 class AccessionRoutine(Routine):
-    """Transforms accession data stored in Ursa Major and delivers the
-       transformed data to ArchivesSpace where it is saved as an accession record."""
+    """Transforms and saves accession data."""
 
     start_status = Package.SAVED
     end_status = Package.ACCESSION_CREATED
@@ -87,34 +98,35 @@ class AccessionRoutine(Routine):
         if not package.accession_data:
             package.accession_data = self.ursa_major_client.retrieve(package.data["accession"])
         package.accession_data["data"]["accession_number"] = self.aspace_client.next_accession_number()
+        package.accession_data["data"]["linked_agents"] = self.get_linked_agents(
+            package.accession_data["data"]["creators"] + [
+                {"name": package.accession_data["data"]["organization"], "type": "organization"}])
         return package.accession_data["data"]
 
     def save_transformed_object(self, transformed):
         if not transformed.get("archivesspace_identifier"):
-            return self.aspace_client.create(transformed, 'accession').get('uri')
+            return self.aspace_client.create(transformed, "accession").get("uri")
 
     def post_save_actions(self, package, full_data, transformed, accession_uri):
-        package.accession_data['data']['archivesspace_identifier'] = accession_uri
-        package.accession_data['data']['accession_number'] = full_data.get("accession_number")
-        for p in package.accession_data['data']['transfers']:
-            for sibling in Package.objects.filter(bag_identifier=p['identifier']):
+        package.accession_data["data"]["archivesspace_identifier"] = accession_uri
+        package.accession_data["data"]["accession_number"] = full_data.get("accession_number")
+        for p in package.accession_data["data"]["transfers"]:
+            for sibling in Package.objects.filter(bag_identifier=p["identifier"]):
                 sibling.accession_data = package.accession_data
                 sibling.save()
 
     def discover_sibling_data(self, package):
         if Package.objects.filter(
-                data__accession=package.data['accession'], accession_data__isnull=False).exists():
+                data__accession=package.data["accession"], accession_data__isnull=False).exists():
             sibling = Package.objects.filter(
-                data__accession=package.data['accession'], accession_data__isnull=False)[0]
+                data__accession=package.data["accession"], accession_data__isnull=False)[0]
             package.accession_data = sibling.accession_data
-            package.data['data']['archivesspace_parent_identifier'] = \
-                sibling.data['data'].get('archivesspace_parent_identifier')
+            package.data["data"]["archivesspace_parent_identifier"] = \
+                sibling.data["data"].get("archivesspace_parent_identifier")
 
 
 class GroupingComponentRoutine(Routine):
-    """Transforms accession data stored in Ursa Major into a grouping component
-       and delivers the transformed data to ArchivesSpace where it is saved
-       as an archival object record."""
+    """Transforms and saves grouping component data."""
 
     start_status = Package.ACCESSION_UPDATE_SENT
     end_status = Package.GROUPING_COMPONENT_CREATED
@@ -125,22 +137,24 @@ class GroupingComponentRoutine(Routine):
     def get_data(self, package):
         data = package.accession_data["data"]
         data["level"] = "recordgrp"
+        data["linked_agents"] = self.get_linked_agents(
+            data["creators"] + [
+                {"name": data["organization"], "type": "organization"}])
         return data
 
     def save_transformed_object(self, transformed):
         if not transformed.get("archivesspace_identifier"):
-            return self.aspace_client.create(transformed, 'component').get('uri')
+            return self.aspace_client.create(transformed, "component").get("uri")
 
     def post_save_actions(self, package, full_data, transformed, parent_uri):
-        for p in package.accession_data['data']['transfers']:
-            for sibling in Package.objects.filter(bag_identifier=p['identifier']):
-                sibling.data['data']['archivesspace_parent_identifier'] = parent_uri
+        for p in package.accession_data["data"]["transfers"]:
+            for sibling in Package.objects.filter(bag_identifier=p["identifier"]):
+                sibling.data["data"]["archivesspace_parent_identifier"] = parent_uri
                 sibling.save()
 
 
 class TransferComponentRoutine(Routine):
-    """Transforms transfer data stored in Ursa Major and delivers the
-       transformed data to ArchivesSpace where it is saved as an archival object record."""
+    """Transforms and saves transfer component data."""
 
     start_status = Package.GROUPING_COMPONENT_CREATED
     end_status = Package.TRANSFER_COMPONENT_CREATED
@@ -150,24 +164,26 @@ class TransferComponentRoutine(Routine):
 
     def get_data(self, package):
         data = package.data["data"]
-        data["resource"] = package.accession_data['data']['resource']
+        data["resource"] = package.accession_data["data"]["resource"]
         data["level"] = "file"
+        data["linked_agents"] = self.get_linked_agents(
+            data["metadata"]["record_creators"] + [
+                {"name": data["metadata"]["source_organization"], "type": "organization"}])
         return data
 
     def save_transformed_object(self, transformed):
         if not transformed.get("archivesspace_identifier"):
-            return self.aspace_client.create(transformed, 'component').get('uri')
+            return self.aspace_client.create(transformed, "component").get("uri")
 
     def post_save_actions(self, package, full_data, transformed, transfer_uri):
-        package.data['data']['archivesspace_identifier'] = transfer_uri
+        package.data["data"]["archivesspace_identifier"] = transfer_uri
         for sibling in Package.objects.filter(bag_identifier=package.bag_identifier):
-            sibling.data['data']['archivesspace_identifier'] = transfer_uri
+            sibling.data["data"]["archivesspace_identifier"] = transfer_uri
             sibling.save()
 
 
 class DigitalObjectRoutine(Routine):
-    """Transforms transfer data stored in Ursa Major and delivers the
-       transformed data to ArchivesSpace where it is saved as a digital object record."""
+    """Transforms and saves digital object data."""
 
     start_status = Package.TRANSFER_COMPONENT_CREATED
     end_status = Package.DIGITAL_OBJECT_CREATED
@@ -179,22 +195,22 @@ class DigitalObjectRoutine(Routine):
         return {"fedora_uri": package.fedora_uri, "use_statement": package.use_statement}
 
     def save_transformed_object(self, transformed):
-        return self.aspace_client.create(transformed, 'digital object').get('uri')
+        return self.aspace_client.create(transformed, "digital object").get("uri")
 
     def post_save_actions(self, package, full_data, transformed, do_uri):
-        transfer_component = self.aspace_client.retrieve(package.data['data']['archivesspace_identifier'])
-        transfer_component['instances'].append(
+        transfer_component = self.aspace_client.retrieve(package.data["data"]["archivesspace_identifier"])
+        transfer_component["instances"].append(
             {"instance_type": "digital_object",
              "jsonmodel_type": "instance",
              "digital_object": {"ref": do_uri}
              })
-        self.aspace_client.update(package.data['data']['archivesspace_identifier'], transfer_component)
+        self.aspace_client.update(package.data["data"]["archivesspace_identifier"], transfer_component)
 
 
 class AuroraUpdater:
-    """
-    Base class for routines that interact with Aurora. Provides a web client
-    and a `run` method.
+    """Base class for routines that interact with Aurora.
+
+    Provides a web client and a `run` method.
 
     To use this class, override the `update_data` method. This method specifies
     the data object to be delivered to Aurora, as well as any changes to that
@@ -204,18 +220,18 @@ class AuroraUpdater:
     """
 
     def __init__(self):
-        self.client = AuroraClient(baseurl=settings.AURORA['baseurl'],
-                                   username=settings.AURORA['username'],
-                                   password=settings.AURORA['password'])
+        self.client = AuroraClient(baseurl=settings.AURORA["baseurl"],
+                                   username=settings.AURORA["username"],
+                                   password=settings.AURORA["password"])
 
     def run(self):
         update_ids = []
-        for obj in Package.objects.filter(process_status=self.start_status, origin='aurora'):
+        for obj in Package.objects.filter(process_status=self.start_status, origin="aurora"):
             try:
                 data = self.update_data(obj)
-                identifier = data['url'].rstrip('/').split('/')[-1]
-                prefix = data['url'].rstrip('/').split('/')[-2]
-                url = "/".join([prefix, "{}/".format(identifier.lstrip('/'))])
+                identifier = data["url"].rstrip("/").split("/")[-1]
+                prefix = data["url"].rstrip("/").split("/")[-2]
+                url = "/".join([prefix, "{}/".format(identifier.lstrip("/"))])
                 self.client.update(url, data=data)
                 obj.process_status = self.end_status
                 obj.save()
@@ -231,8 +247,8 @@ class TransferUpdateRequester(AuroraUpdater):
     end_status = Package.UPDATE_SENT
 
     def update_data(self, obj):
-        data = obj.data['data']
-        data['process_status'] = 90
+        data = obj.data["data"]
+        data["process_status"] = 90
         return data
 
 
@@ -242,6 +258,6 @@ class AccessionUpdateRequester(AuroraUpdater):
     end_status = Package.ACCESSION_UPDATE_SENT
 
     def update_data(self, obj):
-        data = obj.accession_data['data']
-        data['process_status'] = 30
+        data = obj.accession_data["data"]
+        data["process_status"] = 30
         return data
