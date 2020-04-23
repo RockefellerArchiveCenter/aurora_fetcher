f004a130644b38f20e6b78b6db03304a0305ca73
Hillel Arnold <helrond@hotmail.com>
2020-04-23 09:05:51 -0500

    tweak migration;

--- transformer/migrations/0007_auto_20200317_0016.py
+++ transformer/migrations/0007_auto_20200317_0016.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='package',
             name='bag_identifier',
-            field=models.CharField(default=1, max_length=255, unique=True),
+            field=models.CharField(default=1, max_length=255, unique=False),
             preserve_default=False,
         ),
         migrations.AddField(
